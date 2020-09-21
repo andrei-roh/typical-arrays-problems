2d9c52d6a251947b4a8047d52e7ecec78e5ade71
Andrei Roh <andrei_roh@tut.by>
2020-09-21 14:08:30 +0300

    Init: start typical-arrays-problems task
    Added 3 functions in index.js file for solving the task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 
 exports.min = function min (array) {
+  if (!array || !array.length) {
   return 0;
+}
+  return Math.min.apply(null, array)
 }
 
 exports.max = function max (array) {
+  if (!array || !array.length) {
   return 0;
+}
+  return Math.max.apply(null, array)
 }
 
 exports.avg = function avg (array) {
+  if (!array || !array.length) {
   return 0;
+}
+  let arraySum = 0;
+  for (i = 0; i < array.length; i++){
+    arraySum += array[i]
+  }
+  return arraySum / i;
 }
